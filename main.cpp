e2563e0fc9251e22c7a8c2116b5edfba0c899452
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-01-11 23:46:43 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,19 @@
 namespace TelCoColorCoder 
 { 
  
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet" }; 
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]); 
+const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate"}; 
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+
+ std::string ColorPair:: ToString()
+             { 
+                 std::string colorPairStr = MajorColorNames[majorColor]; 
+                 colorPairStr += " "; 
+                 colorPairStr += MinorColorNames[minorColor]; 
+                 return colorPairStr; 
+             } 
 	
 ColorPair GetColorFromPairNumber(int pairNumber) 
 { 
