e86796de20c8d594e10e872b972115da0641d8f7
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-01-11 23:38:41 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@ namespace TelCoColorCoder
 ColorPair GetColorFromPairNumber(int pairNumber) 
 { 
         int zeroBasedPairNumber = pairNumber - 1; 
-        TelCoColorCoder::MajorColor majorColor =  (MajorColor)(zeroBasedPairNumber / numberOfMinorColors); 
-        TelCoColorCoder::MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors); 
+        MajorColor majorColor =  (MajorColor)(zeroBasedPairNumber / numberOfMinorColors); 
+        MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors); 
 		
         return ColorPair(majorColor, minorColor);
 } 
 
-int GetPairNumberFromColor(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor) 
+int GetPairNumberFromColor(MajorColor major, MinorColor minor) 
 { 
          return major * numberOfMinorColors + minor + 1; 
 } 
