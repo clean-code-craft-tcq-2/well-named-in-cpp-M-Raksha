545f71dddbedb00b588a9046c55225fddf065506
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-01-11 22:55:20 +0530

    Update declarations.h

--- declarations.h
+++ declarations.h
@@ -39,3 +39,5 @@ ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(MajorColor major, MinorColor minor);
 void PrintManual();
 }
+void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor);
+void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber);
