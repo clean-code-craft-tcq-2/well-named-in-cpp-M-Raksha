21867208b8efb4dd2096e1aa1828ea2c3f98dc11
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-01-11 23:44:08 +0530

    Update declarations.h

--- declarations.h
+++ declarations.h
@@ -3,10 +3,7 @@ namespace TelCoColorCoder
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET}; 
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE}; 
 
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet" }; 
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]); 
-const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate"}; 
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
   
 class ColorPair 
 { 
@@ -26,13 +23,8 @@ class ColorPair
 			 { 
                  return minorColor; 
            } 
-             std::string ToString()
-             { 
-                 std::string colorPairStr = MajorColorNames[majorColor]; 
-                 colorPairStr += " "; 
-                 colorPairStr += MinorColorNames[minorColor]; 
-                 return colorPairStr; 
-             } 
+             std::string ToString();
+             
 }; 
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(MajorColor major, MinorColor minor);
